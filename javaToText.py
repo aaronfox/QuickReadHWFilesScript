2c84a9fed11a5490d5db3e97b1a1a408f2955362
aaronfox <aaron.m.fox3@gmail.com>
2017-05-18 17:38:56 -0400

    added python scipt

---
+++ javaToText.py
@@ -0,0 +1,34 @@
+#! python3
+#
+# javaToText.py - converts several Java files to one text file
+#
+# NOTE: You must surround the directory and file names with quotes if
+# there are space around their respective path names if you are
+# running this from the command line
+
+import os, sys
+
+# Ensure proper usage
+if len(sys.argv) != 3:
+    print("Usage: python javaToText.py java_directory_to_copy_from text_file_to_write_to")
+    sys.exit()
+
+# Get the directory name from the first argument
+directoryName = sys.argv[1];
+
+# Get the text file name to write to (should end in .txt)
+textFile = sys.argv[2]
+writeTextFile = open(textFile, "w")
+
+if os.path.isdir(directoryName):
+    fileNames = os.listdir(directoryName)
+    for i in range (len(fileNames)):
+        readFile = open(directoryName + "\\" + fileNames[i], "r")
+        writeTextFile.write(readFile.read() + "\n\n\n")
+        print("Writing the text from " + fileNames[i] + "...")
+    print("Success!")
+else:
+    print("Directory not found. :(")
+
+
+
